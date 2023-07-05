f614275b713afa957d5507d59c798d8b01e8d1a4
Miguel CS Vanzela <miguelclaudiodasilvavanzela@gmail.com>
2023-07-05 20:20:10 -0300

    Conserto rápido no arquivo App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Header from "./components/header"
+import Header from "./components/Header";
 import Form from "./components/Form";
 import "./global.css";
 
